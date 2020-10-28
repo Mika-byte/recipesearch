e176841508c435cc18a7f978be27445ba3abe9d4
michaelle <michaelle.barbor@outlook.com>
2020-10-28 01:24:44 -0400

    defined recipe method to ask user enter input to continue or exit program

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,8 @@
+
+require 'pry'
+require 'net/http'
+require 'json'
+
 class API
 
     def self.fetch_results
@@ -5,6 +10,10 @@ class API
         uri = URI(url)
         response = Net::HTTP.get(uri)
         hash = JSON.parse(response)
-
-        
+        binding.pry
+        array_of_results = hash["results"]
+        result.title = result_hash["title"]
+            result.ingredients = result_hash["ingredients"]
+            result.thumbnail = result_hash["thumbnail"]
+    end
 end
--- lib/cli.rb
+++ lib/cli.rb
@@ -6,59 +6,6 @@ class CLI
     end
 
     def recipe
-    puts "Are you looking for recipes?"
-    puts "Type 'yes' to continue or any other key to exit the program"
-
-    user_input = gets.strip.downcase
-
-    if user_input == "yes" || user_input == "y"
-        puts "Awesome!"
-        #display the recipes
-        display_results
-        ask_user_for_type_of_recipe
-
-        sleep(2)
-        puts "\n"
-
-        recipe
-    else 
-        puts "ByeBye"
-    end
-
-end 
-
-def display_results
-    # Access all resulss
-    # binding.pry
-    # print each one out
-    Result.all.each.with_index(1) do |result, index|
-        puts "#{index}. #{result.name}"
-    end 
-end
-
-def ask_user_for_type_of_recipe
-    # ask user for choice
-    puts "Enter the number of the recipe you'd like to know more about"
-    index = gets.strip.to_i - 1
-
-    # index valid? number between 0 and 6
-    until index.between?(0, Result.all.length - 1)
-        # keep asking for user input
-        puts "Sorry invalid. Choose a valid number"
-        index = gets.strip.to_i - 1
-    end
-
-    result_instance = Result.all[index]
-       
-    display_recipe_details(result_instance)
-end 
-
-def display_recipe_details(result)
-    sleep(1)
-    puts "\n"
-    puts result.name 
-    puts "Title: " + result.title
-    puts "Ingredients: " + results.ingredients
-    puts "Thumbnail: " + results.thumbnail
-end
+        puts "Are you looking for recipes?"
+        puts "Type 'yes' to continue or any other key to exit the program"
 end
