24164b9fe4604466193d52af445dbc3cf358d3b5
michaelle <michaelle.barbor@outlook.com>
2020-10-28 01:28:11 -0400

    entered dispaly message after user input yes for answer

--- lib/cli.rb
+++ lib/cli.rb
@@ -8,4 +8,12 @@ class CLI
     def recipe
         puts "Are you looking for recipes?"
         puts "Type 'yes' to continue or any other key to exit the program"
+
+        user_input = gets.strip.downcase
+
+        if user_input == "yes" || user_input == "y"
+            puts "Awesome. Let's find something delicious to cook!"
+            #display the recipes
+            display_results
+            ask_user_for_type_of_recipe
 end
