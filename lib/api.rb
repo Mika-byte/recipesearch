10359728e586d923ead0c0724f2b6773c7eaf621
michaelle <michaelle.barbor@outlook.com>
2020-10-28 01:09:05 -0400

    Building the API class and add information to my API

--- lib/api.rb
+++ lib/api.rb
@@ -1,3 +1,10 @@
 class API
 
+    def self.fetch_results
+        url = "http://www.recipepuppy.com/api/"
+        uri = URI(url)
+        response = Net::HTTP.get(uri)
+        hash = JSON.parse(response)
+
+        
 end
--- lib/result.rb
+++ lib/result.rb
@@ -1,3 +1,14 @@
 class Result
+    attr_accessor :title, :ingredients, :thumbnail
+
+    @@all = []
+
+    def initialize
+        @@all << self
+    end
+
+    def self.all
+        @@all 
+    end
 
 end
